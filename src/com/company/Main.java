24bd9f7fa1ce729d43d01300bdaf3cfe2b83163e
d1saabl3 <73097704+d1saabl3@users.noreply.github.com>
2021-03-02 13:06:55 +0100

    pokus o vylepseni, nasledne jsem si rekl ze ne

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,8 +9,6 @@ public class Main {
 
         ArrayList<String[]> zvirata = new ArrayList<>();
 
-
-
         try {
             BufferedReader br = new BufferedReader(new FileReader("animals.csv"));
 
@@ -22,39 +20,20 @@ public class Main {
 
                 nextLine = br.readLine();
             }
-            for (int i = 0; i < zvirata.size(); i++) {
-                if (zvirata.get(i)[1].equals("0")){
-                    zvirata.get (i)[1] = "Dog";
-                } else if (zvirata.get (i)[1].equals("1")){
-                    zvirata.get(i)[1] = "Cat";
-                } else if (zvirata.get (i)[1].equals("2")){
-                    zvirata.get(i)[1] = "Donkey";
-                } else if (zvirata.get (i)[1] .equals( "3")){
-                    zvirata.get(i)[1] = "Parrot";
-                } else if (zvirata.get (i)[1] .equals( "4")){
-                    zvirata.get(i)[1] = "Horse";
-                } else if (zvirata.get (i)[1] .equals( "5")){
-                    zvirata.get(i)[1] = "Fox";
-                } else if (zvirata.get (i)[1] .equals( "6")){
-                    zvirata.get(i)[1] = "Lion";
-                }else if (zvirata.get (i)[1] .equals( "7")){
-                    zvirata.get(i)[1] = "Tiger";
-                }else if (zvirata.get (i)[1] .equals( "8")){
-                    zvirata.get(i)[1] = "Lizard";
-                }else if (zvirata.get (i)[1] .equals( "9")){
-                    zvirata.get(i)[1] = "Bear";
-                }
 
-                if (zvirata.get(i)[1].equals("TYPE")){
-                    zvirata.get(i)[1] = "TYP_ZVÍŘETE";
-                }
-                if (zvirata.get(i)[2].equals("NAME")){
-                    zvirata.get(i)[2] = "JMÉNO_ZVÍŘETE";
-                }
-
-            }
         }catch (IOException e){
             e.printStackTrace();
+        }
+        for (int i = 0; i < zvirata.size(); i++) {
+
+
+            if (zvirata.get(i)[1].equals("TYPE")){
+                zvirata.get(i)[1] = "TYP_ZVÍŘETE";
+            }
+            if (zvirata.get(i)[2].equals("NAME")){
+                zvirata.get(i)[2] = "JMÉNO_ZVÍŘETE";
+            }
+
         }
         try {
             BufferedWriter bw = new BufferedWriter(new FileWriter("ukol1/ukol1.txt"));
@@ -72,14 +51,5 @@ public class Main {
         } catch (IOException e){
             e.printStackTrace();
         }
-
-
-
-
-
-
-
-
-
     }
 }
